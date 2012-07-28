25752f5be54cd799d2f1390977f1940287e23634
Vagner Zampieri <vgzampieri@gmail.com>
2012-07-28 16:21:05 -0300

    Change controller

--- lib/generators/scaffold_admin/templates/controller.rb
+++ lib/generators/scaffold_admin/templates/controller.rb
@@ -4,7 +4,7 @@ module <%= module_name %>
 class <%= plural_class %>Controller < ApplicationController
 
   layout 'admin'
-  respond_to :html, :json, :js
+  respond_to :html, :json
 
   def index
     @<%= plural_name %> = <%= class_name %>.order 'created_at DESC'
@@ -31,23 +31,15 @@ class <%= plural_class %>Controller < ApplicationController
   def create
     @<%= singular_name %> = <%= class_name %>.new params[:<%= singular_name %>]
 
-    if @<%= singular_name %>.save
-      flash[:notice] = t :<%= singular_name %>_created
-      respond_with @<%= singular_name %>
-    else
-      render :action => :new
-    end
+    flash[:notice] = t :<%= singular_name %>_created if @<%= singular_name %>.save
+    respond_with @<%= singular_name %>
   end
 
   def update
     @<%= singular_name %> = get_register(params[:id])
 
-    if @<%= singular_name %>.update_attributes params[:<%= singular_name %>]
-      flash[:notice] = t :<%= singular_name %>_updated
-      respond_with @<%= singular_name %>
-    else
-      render :action => :edit
-    end
+    flash[:notice] = t :<%= singular_name %>_updated if @<%= singular_name %>.update_attributes params[:<%= singular_name %>]
+    respond_with @<%= singular_name %>
   end
 
   def destroy
